b15a3fc129e1491d767eb046030f0a46fc20b581
Mañana <Maana@DESKTOP-HQBCLRT>
2023-03-09 09:09:31 +0100

    09-03-2023

--- src/matrices/Caracol.java
+++ src/matrices/Caracol.java
@@ -5,17 +5,62 @@ public class Caracol {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int filas = 5;
-		int columnas = 5;
-		int numero = filas*columnas;
-		int[][] matriz = new int[filas][columnas];
+		int tamanio = 5;
+//		int columnas = 5;
+//		int numero = filas*columnas;
+		int[][] matriz = new int[tamanio][tamanio];
 		
-		
-		matriz[2][2] = 1;
-		for(int i=0; i<filas; i++) {
-			for(int j=0; j<columnas; j++) {
-				matriz[0][0] = numero;
+		int I = tamanio/2;
+		int J = tamanio/2;
+
+		int numeroPoner = 1;
+		matriz[I][J]  = numeroPoner++;
+		//ponemos I--/I++/J--/J++ para que la variable se vaya moviendo pero no se puede porque no se puede automatizar 
+//		I--;
+//		matriz[I][J]  = numeroPoner++;
+//		J++;
+//		matriz[I][J]  = numeroPoner++;
+//		I++;
+//		matriz[I][J]  = numeroPoner++;
+		int avance = 2;
+		while(numeroPoner-1 != tamanio*tamanio) {
+			I--;
+			J--;
+			
+			for(int k = 0; k<avance; k++) {
 				
+				J++;
+				matriz[I][J] = numeroPoner++;
+			}
+			for(int k = 0; k<avance; k++) {
+				
+				I++;
+				matriz[I][J] = numeroPoner++;
+			}
+			
+			for(int k = 0; k<avance; k++) {
+				
+				J--;
+				matriz[I][J] = numeroPoner++;
+			}
+			
+			for(int k = 0; k<avance; k++) {
+				
+				I--;
+				matriz[I][J] = numeroPoner++;
+			}
+			avance += 2;
+		}
+		
+
+		 
+//		matriz[2][2] = 1;
+		for(int i=0; i<tamanio; i++) {
+			for(int j=0; j<tamanio; j++) {
+//				matriz[0][0] = numero;
+				if(matriz[i][j] < 10) {
+					System.out.print("0");
+				}
 				System.out.print(matriz[i][j] + " ");
 				
 			}
